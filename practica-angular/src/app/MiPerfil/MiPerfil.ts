7ea4ca426cbb361bb687be1ce643ecaf8f33aea2
SebasCh-07 <sebasch.est@gmail.com>
2025-11-05 09:16:41 -0500

    Reto 4

---
+++ practica-angular/src/app/MiPerfil/MiPerfil.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'mi-perfil',
+    templateUrl: "MiPerfil.html",
+    styleUrl: 'MiPerfil.css'
+    
+})
+
+export class MiPerfil {}
--- practica-angular/src/app/app.ts
+++ practica-angular/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MiPerfil } from './MiPerfil/MiPerfil';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ MiPerfil],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
