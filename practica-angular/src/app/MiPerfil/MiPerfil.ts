8f14714b875fe048fdce358c6ca40eb6e2c49da8
SebasCh-07 <sebasch.est@gmail.com>
2025-11-09 18:17:48 -0500

    Reto 9

--- practica-angular/src/app/MiPerfil/MiPerfil.ts
+++ practica-angular/src/app/MiPerfil/MiPerfil.ts
@@ -18,6 +18,7 @@ export class MiPerfil {
     public nombre: string;
     public edad: number;
     public ocupacion: string;
+    public direccion: string;
 
     constructor() {
         this.user = "S3B45";
@@ -25,9 +26,20 @@ export class MiPerfil {
         this.nombre = "Sebastián Chamorro";
         this.edad = 18;
         this.ocupacion = "Desarrollador de Software";
+        this.direccion = "Calle Falsa 123, Ciudad, País";
 
 
         console.log("MiPerfil component ha sido inicializado.");
         alert("Bienvenido al perfil, estás dentro de la aplicación.");
+    }
+
+    ngDoCheck() {
+        console.log("MiPerfil component ha sido actualizado.");
+    }
+    cambiarEdad() {
+        this.edad = 21;
+    }
+    cambiarDireccion() {
+        this.direccion = "Avenida Siempre Viva 742, Ciudad, País";
     }
 }
