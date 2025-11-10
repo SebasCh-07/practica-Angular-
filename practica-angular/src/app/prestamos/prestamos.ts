109e6101b501f8619275cc1c90a1f852e5dba610
SebasCh-07 <sebasch.est@gmail.com>
2025-11-09 20:48:09 -0500

    Reto12

--- practica-angular/src/app/prestamos/prestamos.ts
+++ practica-angular/src/app/prestamos/prestamos.ts
@@ -10,6 +10,15 @@ export class Prestamos {
   web= "https://www.youtube.com"
   redirigir = true;
 
+  prestamosList = [
+    {id: 1, monto: 100, estado: "Aprobado", valor: true},
+    {id: 2, monto: 200, estado: "Pendiente" , valor: false},
+    {id: 3, monto: 300, estado: "Pendiente", valor: false},
+    {id: 4, monto: 400, estado: "Aprobado", valor: true},
+    {id: 5, monto: 500, estado: "Aprobado", valor: true},
+    {id: 6, monto: 500, estado: "Pendiente", valor: false},
+  ];
+
   cambiarRedirigir(){
     this.redirigir = !this.redirigir;
   }
